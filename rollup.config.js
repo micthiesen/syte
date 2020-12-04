08a016c5c8cad7cd53cb7ffbf13ba1ea7ef95cdb
Michael Thiesen <micthiesen@gmail.com>
2020-12-04 15:20:53 -0800

    Replace with template
    https://github.com/colinbate/svelte-ts-tailwind-template

--- rollup.config.js
+++ rollup.config.js
@@ -1,137 +1,81 @@
-import path from "path";
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import url from "@rollup/plugin-url";
-import svelte from "rollup-plugin-svelte";
-import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import sveltePreprocess from "svelte-preprocess";
-import typescript from "@rollup/plugin-typescript";
-import config from "sapper/config/rollup.js";
-import pkg from "./package.json";
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
+import css from 'rollup-plugin-css-only';
 
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const production = !process.env.ROLLUP_WATCH;
 
-const onwarn = (warning, onwarn) =>
-  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
-  (warning.code === "CIRCULAR_DEPENDENCY" &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
-  warning.code === "THIS_IS_UNDEFINED" ||
-  onwarn(warning);
+function serve() {
+	let server;
+	
+	function toExit() {
+		if (server) server.kill(0);
+	}
 
-const preprocess = sveltePreprocess();
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
 
 export default {
-  client: {
-    input: config.client.input().replace(/\.js$/, ".ts"),
-    output: config.client.output(),
-    plugins: [
-      replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode),
-      }),
-      svelte({
-        dev,
-        hydratable: true,
-        preprocess: preprocess,
-        emitCss: true,
-      }),
-      url({
-        sourceDir: path.resolve(__dirname, "src/node_modules/images"),
-        publicPath: "/client/",
-      }),
-      resolve({
-        browser: true,
-        dedupe: ["svelte"],
-      }),
-      commonjs(),
-      typescript({ sourceMap: dev }),
+	input: 'src/main.ts',
+	output: {
+		sourcemap: !production,
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/bundle.js'
+	},
+	plugins: [
+		svelte({
+			// enable run-time checks when not in production
+			compilerOptions: {
+				dev: !production,
+			},
+			preprocess: sveltePreprocess({
+				postcss: true,
+			}),
+		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+		typescript({ sourceMap: !production }),
 
-      legacy &&
-        babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
-          babelHelpers: "runtime",
-          exclude: ["node_modules/@babel/**"],
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: "> 0.25%, not dead",
-              },
-            ],
-          ],
-          plugins: [
-            "@babel/plugin-syntax-dynamic-import",
-            [
-              "@babel/plugin-transform-runtime",
-              {
-                useESModules: true,
-              },
-            ],
-          ],
-        }),
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
 
-      !dev &&
-        terser({
-          module: true,
-        }),
-    ],
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
-    preserveEntrySignatures: false,
-    onwarn,
-  },
-
-  server: {
-    input: { server: config.server.input().server.replace(/\.js$/, ".ts") },
-    output: config.server.output(),
-    plugins: [
-      replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode),
-      }),
-      svelte({
-        generate: "ssr",
-        hydratable: true,
-        preprocess: preprocess,
-        dev,
-      }),
-      url({
-        sourceDir: path.resolve(__dirname, "src/node_modules/images"),
-        publicPath: "/client/",
-        emitFiles: false, // already emitted by client build
-      }),
-      resolve({
-        dedupe: ["svelte"],
-      }),
-      commonjs(),
-      typescript({ sourceMap: dev }),
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules
-    ),
-
-    preserveEntrySignatures: "strict",
-    onwarn,
-  },
-
-  serviceworker: {
-    input: config.serviceworker.input().replace(/\.js$/, ".ts"),
-    output: config.serviceworker.output(),
-    plugins: [
-      resolve(),
-      replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode),
-      }),
-      commonjs(),
-      typescript({ sourceMap: dev }),
-      !dev && terser(),
-    ],
-
-    preserveEntrySignatures: false,
-    onwarn,
-  },
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
 };
