0fa3b28785124c16a59e17770e16db382cbd85ab
Michael Thiesen <michael.thiesen@canalyst.com>
2021-02-27 13:04:10 -0800

    Add prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-	import "smelte/src/tailwind.css";
-	export let name: string;
+  import "smelte/src/tailwind.css";
+  export let name: string;
 </script>
-<style>
-	.custom-style {
-		@apply italic;
-	}
-</style>
+
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
-	<p class="custom-style">
-		Visit the
-		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
+  <h1 class="uppercase text-6xl leading-normal font-thin text-svelte">
+    Hello {name}!
+  </h1>
+  <p class="custom-style">
+    Visit the
+    <a href="https://svelte.dev/tutorial" class="text-blue-500 underline"
+      >Svelte tutorial
+    </a>
+    to learn how to build Svelte apps.
+  </p>
 </main>
+
+<style>
+  .custom-style {
+    @apply italic;
+  }
+</style>
