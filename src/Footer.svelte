401bd4772b7e58b9caa81bd2cc5ab6634e0b90e3
Michael Thiesen <michael.thiesen@canalyst.com>
2021-02-27 15:54:11 -0800

    Run linting via Vercel

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 </script>
 
-<div
-  class="fixed bottom-0 left-0 right-0 p-2.5 px-4 flex flex-row flex-wrap text-gray-400">
+<footer
+  class="fixed bottom-0 left-0 right-0 p-2.5 px-4 flex flex-row flex-wrap bg-gray-900 text-gray-300">
   <h1 class="mr-auto self-end text-1xl pr-4">Software Engineer, Vancouver</h1>
-  <h1 class="uppercase text-4xl font-light">Michael Thiesen</h1>
-</div>
+  <h1 class="uppercase text-3xl font-light">Michael Thiesen</h1>
+</footer>
 
 <style></style>
